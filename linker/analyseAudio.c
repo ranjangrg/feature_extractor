81853846e7c140efe66547a143e5e98d884229fe
ranjangrg <ranjan_grg@hotmail.co.uk>
2019-06-12 16:39:40 +0200

    Initial files

---
+++ linker/analyseAudio.c
@@ -0,0 +1,7 @@
+#ifndef __ANALYSE_AUDIO_H
+	#include "analyseAudio.h"
+#endif
+
+int main() {
+	return 1;
+}
---
+++ linker/analyseAudio.h
@@ -0,0 +1,6 @@
+#ifndef __ANALYSE_AUDIO_H
+	#define __ANALYSE_AUDIO_H
+
+	#include <stdio.h>
+
+#endif
